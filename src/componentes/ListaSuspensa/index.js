f3738cadc58598516f343fc8cf06ba94ffc92dc8
Ramon Simões <84634831+ramonsimoes@users.noreply.github.com>
2025-01-26 10:48:50 -0300

    Organo
    260125

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,25 @@
+import './ListaSuspensa.css';
+
+const ListaSuspensa = (props) => {
+
+    
+
+    return (
+        <div className='lista-suspensa'>
+            <label>
+                {props.label}
+            </label>
+            <select required={props.obrigatorio} valor={props.valor} onChange={evento => props.aoAlterado(evento.target.value)}>
+                {props.itens.map
+                (item => {
+                    return <option key={item}>{item}</option>
+                }
+                )
+                }
+            </select>
+        </div>
+    );
+}
+
+export default ListaSuspensa;
+
